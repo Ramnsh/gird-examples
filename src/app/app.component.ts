0dc7d5ed5b5f7cae235304275bea85510562d2f9
ramnsh <ramneek.balotra@gmail.com>
2017-10-31 09:14:13 +0530

    add/delete function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { UserInfo } from './user-info';
 
 @Component({ 
   selector: 'app-component',
-  template: `
-    <h1>Hello Git</h1>
-  `
+  templateUrl: 'app/app-component.html'
 })
-export class AppComponent {
+export class AppComponent  implements  OnInit {
   
-  constructor() {
+  user: Array<UserInfo> = [];
+  checked: boolean = false;
+  counter: number;
+  constructor(private ele: ElementRef) {
 
   }
+
+  ngOnInit() {
+    console.log(this.ele.nativeElement);
+    this.counter = 1;
+   
+    this.user.push({
+      Id: this.counter,
+      Name:  ' ',
+      Mail: ' ',
+      Mobile: ' '
+
+    });
+
+  }
+
+  AddRow () {
+
+    this.counter ++;
+
+    this.user.push({
+      Id: this.counter,
+      Name:  ' ',
+      Mail: ' ',
+      Mobile: ' '
+
+    });
+  }
+ DeleteRow (u: UserInfo) {
+  
+  if (this.checked) {
+    var index = this.user.indexOf(u);
+    this.user.splice(index,1);
+  } else {
+    console.log('select the row');
+  }
+   
+  }
+
+  SelectedEle (e:any) {
+
+    if (e.target.checked) {
+      this.checked = true;
+  }
+
+
   
 }
